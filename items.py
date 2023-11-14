43eef4aeed281f4057ed4a6c16b0a75f6c09cbe3
Jesse_A <146388635+mugeltun@users.noreply.github.com>
2023-11-14 16:36:27 -0500

    Added items
    new item health potion added

--- items.py
+++ items.py
@@ -6,14 +6,16 @@ class Items():
         self.type = type
         self.durability = durability
 
-    def clock():
-        return Items("clock","utility",99999)
+    def book():
+        return Items("strange_book","book",99999)
     def blade():
         return Items("blade","weapon",12)
     def holy_water():
         return Items("holy water","unkown",1)
     def torch():
         return Items("torch","utility",99999)
+    def health_potion():
+        return Items("health potion","utility",1)
     def holy_blade():
         return Items("holy blade","weapon",99999)
     def lament_puzzle():
@@ -25,20 +27,3 @@ class Items():
     def item_here_hint():
         print("There is an item somewhere here!!!")
 
-
-
-    
-
-# item_present = True
-
-# while item_present:
-#     item_select = input("you find something on the floor it seems to be a clock? pick it up (y/n) ")
-#     if item_select == "y":
-#         print(clock)
-#         item_present = None
-#     elif item_select == "n":
-#         print("you walk past it...")
-#         item_present = None
-#     else:
-#         print("try again only press y/n!")
-#         item_present = True
