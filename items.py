21cf67a55d4a12377d3cf29d25210a022b6a46db
Jesse_A <146388635+mugeltun@users.noreply.github.com>
2023-11-15 22:07:20 -0500

    New Item instance attribute added!
    added item damage to the class for weapons

--- items.py
+++ items.py
@@ -1,27 +1,28 @@
 # this will have all the items that are available to the player
 
 class Items():
-    def __init__(self,name,type,durability):
+    def __init__(self,name,type,durability,damage):
         self.name = name
         self.type = type
         self.durability = durability
+        self.damage = damage
 
     def book():
-        return Items("strange_book","book",99999)
+        return Items("strange book","utility",99999,0)
     def blade():
-        return Items("blade","weapon",12)
+        return Items("blade","weapon",12,60)
     def holy_water():
-        return Items("holy water","unkown",1)
+        return Items("holy water","unkown",1,150)
     def torch():
-        return Items("torch","utility",99999)
+        return Items("torch","utility",99999,5)
     def health_potion():
-        return Items("health potion","utility",1)
+        return Items("health potion","utility",1,0)
     def holy_blade():
-        return Items("holy blade","weapon",99999)
+        return Items("holy blade","weapon",99999,120)
     def lament_puzzle():
-        return Items("lament","puzzle piece",1)
+        return Items("lament","puzzle piece",1,0)
     def completed_lament():
-        return Items("completed lament","completed puzzle",1)
+        return Items("completed lament","completed puzzle",1,0)
     def lament_puzzle_piece_found():
         print("you have found one part of the lament puzzle!")
     def item_here_hint():
